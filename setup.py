f25c4fe4d218a2e8040aa90c337256c82557b70d
Mark Ferguson <mark@rareearthapps.com>
2018-12-14 20:20:08 -0700

    Corrected import paths so that pip works properly

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 """This module contains the packaging routine for the ``scrapy-algolia-exporter`` package"""
 
 from setuptools import setup, find_packages
-from pip.download import PipSession
-from pip.req import parse_requirements
+from pip._internal.download import PipSession
+from pip._internal.req import parse_requirements
 
 
 def get_requirements(source):
